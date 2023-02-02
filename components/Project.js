674878afdf10991430e0ecebf7e61eea5efc4f47
Yebincho03 <lucycho0314@gmail.com>
2023-02-01 23:24:23 -0800

    add project component

---
+++ components/Project.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styled, { keyframes } from "styled-components";
+
+const Project = (props) => {
+  const { icon, alt } = props;
+
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Project;
