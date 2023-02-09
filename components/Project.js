6581251154480e96ded0168dee566e20253e09e8
Yebincho03 <lucycho0314@gmail.com>
2023-02-08 22:40:43 -0800

    changes

--- components/Project.js
+++ components/Project.js
@@ -4,13 +4,21 @@ import styled, { keyframes } from "styled-components";
 const Project = (props) => {
   const { icon, alt } = props;
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className="wrapper"></div>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .wrapper {
+    border: 1pm solid white;
+  }
 `;
 
 export default Project;
